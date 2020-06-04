c2072714a0ed7220752bbbec34816d58e065dc1c
espelu <espen.lunden@gmail.com>
2020-06-04 15:35:48 +0200

    input wrench not working

--- etasl_ros_control_examples/scripts/example_kuka_force.lua
+++ etasl_ros_control_examples/scripts/example_kuka_force.lua
@@ -23,50 +23,87 @@ robot_joints = {
     "joint_a6"
 }
 
-
-meas_fx = ctx:createInputChannelScalar("etasl_controller/meas_fx") --/etasl_controller/
-meas_fy = ctx:createInputChannelScalar("etasl_controller/meas_fy")
-meas_fz = ctx:createInputChannelScalar("etasl_controller/meas_fz")
--- fx = constant(10)
--- fy = constant(10)
--- fz = constant(10)
+calib_wrench = ctx:createInputChannelWrench("calib_wrench")
 
 -- Force constraints
 Constraint{
     context = ctx,
     name    = "fx_control",
     model   = coord_x(origin(robot_ee)),
-    meas    = meas_fx, --Fmeas_ee,a
+    meas    = calib_wrench[1], --Fmeas_ee,a
     target  = constant(0), --Fdes_ee,
     priority= 2,
-    K       = 0.001 --1/Damping constant
+    K       = 0.001 --Usikker på K
 }
 
 Constraint{
     context = ctx,
     name    = "fy_control",
     model   = coord_y(origin(robot_ee)),
-    meas    = meas_fy, --Fmeas_ee,
-    target  = constant(0), --Fdes_ee, 
+    meas    = calib_wrench[2], --Fmeas_ee,
+    target  = 0, --Fdes_ee, 
     priority= 2,
-    K       = 0.001 --1/Damping constant
+    K       = 0.001 --Usikker på K
 }
 
 Constraint{
     context = ctx,
     name    = "fz_control",
     model   = coord_z(origin(robot_ee)),
-    meas    = meas_fz, --Fmeas_ee,
-    target  = constant(0), --Fdes_ee, 
+    meas    = calib_wrench[3], --Fmeas_ee,
+    target  = 0, --Fdes_ee, 
     priority= 2,
-    K       = 0.001 --1/Damping constant
+    K       = 0.001 --Usikker på K
 }
 
+ctx:setOutputExpression("error_fx", calib_wrench[1] - 0)
+ctx:setOutputExpression("error_fy", calib_wrench[2] - 0)
+ctx:setOutputExpression("error_fz", calib_wrench[3] - 0)
 
 
-ctx:setOutputExpression("error_fx", meas_fx - 0)
-ctx:setOutputExpression("error_fy", meas_fy - 0)
-ctx:setOutputExpression("error_fz", meas_fz - 0)
+-- -- meas_fx = ctx:createInputChannelScalar("etasl_controller/meas_fx") --/etasl_controller/
+-- -- meas_fy = ctx:createInputChannelScalar("etasl_controller/meas_fy")
+-- -- meas_fz = ctx:createInputChannelScalar("etasl_controller/meas_fz")
+-- -- fx = constant(10)
+-- -- fy = constant(10)
+-- -- fz = constant(10)
+
+-- -- Force constraints
+-- Constraint{
+--     context = ctx,
+--     name    = "fx_control",
+--     model   = coord_x(origin(robot_ee)),
+--     meas    = meas_fx, --Fmeas_ee,a
+--     target  = constant(0), --Fdes_ee,
+--     priority= 2,
+--     K       = 0.001 --1/Damping constant
+-- }
+
+-- Constraint{
+--     context = ctx,
+--     name    = "fy_control",
+--     model   = coord_y(origin(robot_ee)),
+--     meas    = meas_fy, --Fmeas_ee,
+--     target  = constant(0), --Fdes_ee, 
+--     priority= 2,
+--     K       = 0.001 --1/Damping constant
+-- }
+
+-- Constraint{
+--     context = ctx,
+--     name    = "fz_control",
+--     model   = coord_z(origin(robot_ee)),
+--     meas    = meas_fz, --Fmeas_ee,
+--     target  = constant(0), --Fdes_ee, 
+--     priority= 2,
+--     K       = 0.001 --1/Damping constant
+-- }
+
+
+
+-- ctx:setOutputExpression("error_fx", meas_fx - 0)
+-- ctx:setOutputExpression("error_fy", meas_fy - 0)
+-- ctx:setOutputExpression("error_fz", meas_fz - 0)
 
 
 
